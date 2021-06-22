a733fc847ea3b18c7896980fd67999fefe61ca86
elmaestroguru <elmaestroguru@users.noreply.github.com>
2021-06-22 01:48:12 -0500

    formulario
    prueba

---
+++ procesar.php
@@ -0,0 +1,15 @@
+<?php
+$nombre = $_POST['nombre'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+
+echo "<h2>Informacion recibida desde php</h2>";
+echo "El nombre recibido es:" . $nombre . "<br/>";
+echo "El asunto recibido es:" . $asunto . "<br/>";
+echo "El mensaje recibido es:" . $mensaje . "<br/>";
+
+
+
+
+
+?>
